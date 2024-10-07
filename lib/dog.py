44b795afaad98e1533481427d152dfa8fa9b5c8b
Dov Azogui <dov.azogui@gmail.com>
2024-10-07 14:26:59 -0400

    dog class edited

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,21 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    
+    def __init__(self, name = "tg", breed = "Mastiff"):
+        approved_breeds = ["Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle", "French Bulldog", "Pug", "Pointer"]
+        if (type(name) == str and 0 < len(name)< 26):
+            self.name = name
+        else: 
+            print("Name must be string between 1 and 25 characters.")
+        
+        if breed in approved_breeds:
+            self.breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+            
+            
+    
+        
+        
+    
