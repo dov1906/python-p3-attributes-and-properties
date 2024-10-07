3f45cc694af47b9d96738b2018da251eb9aa873e
Dov Azogui <dov.azogui@gmail.com>
2024-10-07 14:34:00 -0400

    edited the class Person

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,16 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    
+    def __init__(self, name = "tg", job = "Admin"):
+        approved_jobs = ["Admin", "Customer Service", "Human Resources", "ITC", "Production", "Legal", "Finance", "Sales", "General Management", "Research & Development", "Marketing", "Purchasing"]
+        if (type(name) == str and 0 < len(name)< 26):
+            self.name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+        if job in approved_jobs:
+            self.job = job
+        else:
+            print("Job must be in list of approved jobs.")
+            
+
